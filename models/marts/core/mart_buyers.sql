badf56de1a83867466a466f5e1046a86921d7681
Jan Kiljanski <jan.jozef.kiljanski@gmail.com>
2026-03-23 17:10:24 +0100

    Remove starter scaffold models

--- models/marts/core/mart_buyers.sql
+++
@@ -1,9 +0,0 @@
-select
-    buyer_key,
-    buyer_id,
-    buyer_name,
-    case_count,
-    first_case_date,
-    last_case_date,
-    total_notice_count
-from {{ ref('int_procurement__buyers') }}
--- models/marts/core/mart_market_daily.sql
+++
@@ -1,7 +0,0 @@
-select
-    publication_date,
-    count(distinct notice_id) as notice_count,
-    count(distinct case_id) as case_count,
-    count(distinct buyer_id) as buyer_count
-from {{ ref('stg_silver__common_envelope') }}
-group by 1
